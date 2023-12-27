6982946c36dc21a5280138469b8bc0c4207dc15a
nephyrux <dixtraction@gmail.com>
2023-12-26 22:16:18 -0800

    python_test_LINUX

--- Python/Intro.py
+++ Python/Intro.py
@@ -1,2 +1,5 @@
 # ! Time to start a new exciting journey. This is the main file for the rest of the python scripts
 # Lets become the best at this.
+first_string, second_string, exclamation_mark = 'Hello', 'World', '!'
+print( first_string + ' ' + second_string + exclamation_mark )
+
